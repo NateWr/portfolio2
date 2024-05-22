5cbf14ab183cda69c958e4286f8e73568dc2dddc
Nate Wright <natew@notthisway.com>
2024-05-22 13:32:05 +0100

    Add blur effect to portfolio items

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,9 @@
 import nav from "./nav";
 import scrollProgress from "./scroll-progress";
 import scrollPortfolio from "./scroll-portfolio";
+import scrollBlurItems from "./scroll-blur-items";
 
 nav();
-scrollProgress();
-scrollPortfolio();
+scrollProgress()
+scrollPortfolio()
+scrollBlurItems()
---
+++ src/scripts/scroll-blur-items.ts
@@ -0,0 +1,15 @@
+export default () => {
+
+  const SELECTOR = '[data-blur]'
+
+  const callback = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(entry => {
+      const target = <HTMLElement>entry.target
+      target.dataset.blur = entry.isIntersecting ? 'false' : 'true'
+    })
+  }
+
+  const items = <HTMLElement[]>[...document.querySelectorAll(SELECTOR)]
+  const ob = new IntersectionObserver(callback, {threshold: 0.4})
+  items.forEach(item => ob.observe(item))
+}
