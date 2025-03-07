115d69e227ad9a4a15a9e0126517af741be38f9a
Nate Wright <natew@notthisway.com>
2025-03-07 23:17:40 +0000

    Add: intent database to portfolio

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -1,5 +1,6 @@
 import type { PortfolioItem } from "../scripts/portfolio-item"
 import uiLibraryImage from '../images/ui-library-submission-wide.png';
+import intentImage from '../images/intent.png';
 import councillorsImage from '../images/find-your-councillors.png';
 import lawfareImage from '../images/lawfare-google-sheet.png';
 
@@ -14,13 +15,22 @@ const stories : PortfolioItem[] = [
     alt: "Screenshot of the step-by-step workflow to make a new journal submission",
   },
   {
+    title: "Intent",
+    client: "Law for Palestine",
+    date: "Oct 2024",
+    url: "/intent",
+    image: intentImage,
+    color: "#b81e42",
+    alt: "Screenshot of the database of statements of intent to commit genocide",
+  },
+  {
     title: "Find Your City Councillor",
     client: "Side Project",
     date: "June 2023",
     url: "/find-your-representatives",
     image: councillorsImage,
     color: "#B50A00",
-    alt: "Screenshot of the a map of Edinburgh with details about the city councillors",
+    alt: "Screenshot of a map of Edinburgh with details about the city's councillors",
   },
   {
     title: "Lawfare Data Extraction",
