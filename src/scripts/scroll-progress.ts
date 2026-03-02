d01bd2dcd9f839addff8c85089252eb87fbf1cff
Nate Wright <natew@notthisway.com>
2026-03-02 14:05:01 +0000

    Fix: scroll progress bar doesn't reach 100

--- src/scripts/scroll-progress.ts
+++ src/scripts/scroll-progress.ts
@@ -12,7 +12,12 @@ export default () => {
   const setProgress = () => {
     const scrollMax = document.documentElement.offsetHeight - document.documentElement.clientHeight
     progress = document.documentElement.scrollTop / scrollMax
-    $progress?.style.setProperty('--width', `${Math.round(progress * 100)}%`)
+    progress = Math.round(progress * 100)
+    // On some screens, the scroll will never quite reach 100
+    if (progress > 97) {
+      progress = 100
+    }
+    $progress?.style.setProperty('--width', `${progress}%`)
   }
 
   window.addEventListener('load', setProgress)
